1ce7bc15bb37bbc8070082f74bb1f91c17abc036
HaikalCannavaro <haikalcannavaro64@gmail.com>
2025-03-16 16:03:45 +0700

    Menambahkan class DataGeneric method PrintData

--- tpmodul5_103022300106/Program.cs
+++ tpmodul5_103022300106/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Haikal Cannavaro");
+
+        DataGeneric<string> data = new DataGeneric<string>("103022300106");
+        data.PrintData();
+
+        Console.ReadKey();
+    }
+}
